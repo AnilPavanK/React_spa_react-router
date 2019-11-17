af778d0b809063763ed047142984ed65aa5377cb
Anil Pavan <appkumark@yahoo.com>
2019-11-18 05:03:15 +0530

    Updates

---
+++ precache-manifest.c3fa2a5e28fb37dc8cea80e0bb978f1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82ede4064980d8ddc9042cabe729b2d",
+    "url": "/spa/index.html"
+  },
+  {
+    "revision": "301f4a46a5c06d24739f",
+    "url": "/spa/static/css/main.9556855e.chunk.css"
+  },
+  {
+    "revision": "dc58970c7ebb4e55f5c0",
+    "url": "/spa/static/js/2.42062d8c.chunk.js"
+  },
+  {
+    "revision": "301f4a46a5c06d24739f",
+    "url": "/spa/static/js/main.d5190c27.chunk.js"
+  },
+  {
+    "revision": "541318a5eab955973b64",
+    "url": "/spa/static/js/runtime-main.f07a6d41.js"
+  }
+]);
